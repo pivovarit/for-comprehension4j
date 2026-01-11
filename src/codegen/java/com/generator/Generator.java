4afe3f1f55b8777d359d0f9901a87b9ffd07b300
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-01-11 12:31:52 +0100

    Generate FunctionN types

--- src/codegen/java/com/generator/Generator.java
+++ src/codegen/java/com/generator/Generator.java
@@ -3,13 +3,20 @@ package com.generator;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Generator {
 
     private static final String PACKAGE_NAME = "com/pivovarit/forc";
+    private static final int ARITY = 2;
 
-    public static void main(String[] args) throws IOException {
+    void main(String[] args) throws IOException {
         var dir = Files.createDirectories(Path.of(args[0], PACKAGE_NAME));
+
+        for (int i = 1; i <= ARITY; i++) {
+            Files.writeString(dir.resolve("Function%d.java".formatted(i)), FunctionTypeGenerator.generate(i));
+        }
 
         Files.writeString(dir.resolve("ForComprehension.java"),
           licenseHeader() +
@@ -18,8 +25,6 @@ public class Generator {
             
             import java.util.Objects;
             import java.util.Optional;
-            import java.util.function.BiFunction;
-            import java.util.function.Function;
             
             /**
              * Provides a fluent for-comprehension API for Java, inspired by Scala's for-expressions.
@@ -80,13 +85,13 @@ public class Generator {
                      *         or {@link Optional#empty()} if any input optional is empty
                      * @throws NullPointerException if the function is {@code null}
                      */
-                    public <R> Optional<R> yield(BiFunction<? super T1, ? super T2, ? extends R> f) {
+                    public <R> Optional<R> yield(Function2<? super T1, ? super T2, ? extends R> f) {
                         Objects.requireNonNull(f, "f is null");
             
                         return o1.flatMap(t1 -> o2.map(t2 -> f.apply(t1, t2)));
                     }
                 }
-
+            
                 /**
                  * Creates a lazy for-comprehension over two {@link Optional} values.
                  * <p>
@@ -100,7 +105,7 @@ public class Generator {
                  * @return a lazy for-comprehension over two optional values
                  * @throws NullPointerException if any argument is {@code null}
                  */
-                public static <T1, T2> ForLazy2Optional<T1, T2> forc(Optional<T1> o1, Function<? super T1, Optional<T2>> o2) {
+                public static <T1, T2> ForLazy2Optional<T1, T2> forc(Optional<T1> o1, Function1<? super T1, Optional<T2>> o2) {
                     Objects.requireNonNull(o1, "o1 is null");
                     Objects.requireNonNull(o2, "o2 is null");
                     return new ForLazy2Optional<>(o1, o2);
@@ -116,13 +121,13 @@ public class Generator {
                 public static final class ForLazy2Optional<T1, T2> {
             
                     private final Optional<T1> o1;
-                    private final Function<? super T1, Optional<T2>> o2;
+                    private final Function1<? super T1, Optional<T2>> o2;
             
-                    private ForLazy2Optional(Optional<T1> o1, Function<? super T1, Optional<T2>> o2) {
+                    private ForLazy2Optional(Optional<T1> o1, Function1<? super T1, Optional<T2>> o2) {
                         this.o1 = o1;
                         this.o2 = o2;
                     }
-
+            
                     /**
                      * Produces the result of the for-comprehension by applying the given function
                      * to the contained values.
@@ -135,7 +140,7 @@ public class Generator {
                      *         or {@link Optional#empty()} if any step yields an empty optional
                      * @throws NullPointerException if the function is {@code null}
                      */
-                    public <R> Optional<R> yield(BiFunction<? super T1, ? super T2, ? extends R> f) {
+                    public <R> Optional<R> yield(Function2<? super T1, ? super T2, ? extends R> f) {
                         Objects.requireNonNull(f, "f is null");
             
                         return o1.flatMap(t1 -> o2.apply(t1).map(t2 -> f.apply(t1, t2)));
@@ -163,5 +168,48 @@ public class Generator {
            * limitations under the License.
            */
           """;
+    }
+
+    static class FunctionTypeGenerator {
+        static String generate(int arity) {
+            return """
+              %spackage com.pivovarit.forc;
+              
+              import java.util.Objects;
+              
+              /**
+               * Represents a function with %d argument%s.
+               */
+              @FunctionalInterface
+              public interface Function%d<%s> {
+              
+                  R apply(%s);
+              
+                  default <V> Function%d<%s> andThen(Function1<? super R, ? extends V> after) {
+                      Objects.requireNonNull(after);
+                      return (%s) -> after.apply(apply(%s));
+                  }
+              }
+              """.formatted(licenseHeader(), arity, arity > 1 ? "s" : "", arity, typeParams(arity, "R"), argList(arity), arity, typeParams(arity, "V"), argNames(arity), argNames(arity));
+        }
+
+        private static String typeParams(int arity, String last) {
+            var params = IntStream.rangeClosed(1, arity)
+              .mapToObj("T%d"::formatted)
+              .collect(Collectors.joining(", "));
+            return params.isEmpty() ? last : params + ", " + last;
+        }
+
+        private static String argList(int arity) {
+            return IntStream.rangeClosed(1, arity)
+              .mapToObj(i -> "T%d t%d".formatted(i, i))
+              .collect(Collectors.joining(", "));
+        }
+
+        private static String argNames(int arity) {
+            return IntStream.rangeClosed(1, arity)
+              .mapToObj("t%d"::formatted)
+              .collect(Collectors.joining(", "));
+        }
     }
 }
